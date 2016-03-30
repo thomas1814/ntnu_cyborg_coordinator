5c754110cd0187d13524067cab9b0f98bdd75da1
Per Magnus Veierland <pveierland@gmail.com>
2016-03-30 15:27:21 +0200

    Namespacing + cleanup

--- src/cyborg_coordinator_node.cpp
+++ src/cyborg_coordinator_node.cpp
@@ -5,94 +5,100 @@
 
 #include <string>
 
-class Coordinator
+namespace cyborg
 {
-    public:
-        Coordinator(ros::NodeHandle& node)
-            : node_(node)
+    namespace coordinator
+    {
+        class Coordinator
         {
-            requestControlService_ = node.advertiseService(
-                "/cyborg_coordinator/requestControl", &Coordinator::requestControl, this);
-            releaseControlService_ = node.advertiseService(
-                "/cyborg_coordinator/releaseControl", &Coordinator::releaseControl, this);
+            public:
+                Coordinator(ros::NodeHandle& node)
+                    : node_(node)
+                {
+                    requestControlService_ = node.advertiseService(
+                        "/cyborg_coordinator/requestControl", &Coordinator::requestControl, this);
+                    releaseControlService_ = node.advertiseService(
+                        "/cyborg_coordinator/releaseControl", &Coordinator::releaseControl, this);
 
-            rosAriaCmdVelPublisher_ = node.advertise<geometry_msgs::Twist>("RosAria/cmd_vel", 1);
-        }
+                    rosAriaCmdVelPublisher_ = node.advertise<geometry_msgs::Twist>("RosAria/cmd_vel", 1);
+                }
 
-        void cmdVel(const geometry_msgs::Twist::ConstPtr& message)
-        {
-            rosAriaCmdVelPublisher_.publish(message);
-        }
+                void cmdVel(const geometry_msgs::Twist::ConstPtr& message)
+                {
+                    rosAriaCmdVelPublisher_.publish(message);
+                }
 
-        bool requestControl(cyborg_coordinator::RequestControl::Request&  request,
-                            cyborg_coordinator::RequestControl::Response& response)
-        {
-            if (currentControllerId_.empty())
-            {
-                ROS_DEBUG_NAMED("cyborg_coordinator",
-                    "requestControl: %s has taken control!", request.id.c_str());
+                bool requestControl(cyborg_coordinator::RequestControl::Request&  request,
+                                    cyborg_coordinator::RequestControl::Response& response)
+                {
+                    if (currentControllerId_.empty())
+                    {
+                        ROS_DEBUG_NAMED("cyborg_coordinator",
+                            "requestControl: %s has taken control!", request.id.c_str());
 
-                const std::string topicBaseName =
-                    std::string("/cyborg_coordinator/") + request.id;
+                        const std::string topicBaseName =
+                            std::string("/cyborg_coordinator/") + request.id;
 
-                rosAriaCmdVelSubscriber_ = node_.subscribe(
-                    topicBaseName + "/RosAria/cmd_vel", 1, &Coordinator::cmdVel, this);
+                        rosAriaCmdVelSubscriber_ = node_.subscribe(
+                            topicBaseName + "/RosAria/cmd_vel", 1, &Coordinator::cmdVel, this);
 
-                currentControllerId_     = request.id;
-                response.controlReceived = true;
-            }
-            else if (request.id == currentControllerId_)
-            {
-                ROS_DEBUG_NAMED("cyborg_coordinator",
-                    "requestControl: %s already has control!", request.id.c_str());
-                response.controlReceived = true;
-            }
-            else
-            {
-                ROS_DEBUG_NAMED("cyborg_coordinator",
-                    "requestControl: %s has been denied control!", request.id.c_str());
-                response.controlReceived = false;
-            }
+                        currentControllerId_     = request.id;
+                        response.controlReceived = true;
+                    }
+                    else if (request.id == currentControllerId_)
+                    {
+                        ROS_DEBUG_NAMED("cyborg_coordinator",
+                            "requestControl: %s already has control!", request.id.c_str());
+                        response.controlReceived = true;
+                    }
+                    else
+                    {
+                        ROS_DEBUG_NAMED("cyborg_coordinator",
+                            "requestControl: %s has been denied control!", request.id.c_str());
+                        response.controlReceived = false;
+                    }
 
-            return true;
-        }
+                    return true;
+                }
 
-        bool releaseControl(cyborg_coordinator::ReleaseControl::Request&  request,
-                            cyborg_coordinator::ReleaseControl::Response& response)
-        {
-            if (request.id == currentControllerId_)
-            {
-                ROS_DEBUG_NAMED("cyborg_coordinator",
-                    "releaseControl: %s gave up control!", request.id.c_str());
+                bool releaseControl(cyborg_coordinator::ReleaseControl::Request&  request,
+                                    cyborg_coordinator::ReleaseControl::Response& response)
+                {
+                    if (request.id == currentControllerId_)
+                    {
+                        ROS_DEBUG_NAMED("cyborg_coordinator",
+                            "releaseControl: %s gave up control!", request.id.c_str());
 
-                currentControllerId_.clear();
-                rosAriaCmdVelSubscriber_.shutdown();
-            }
-            else
-            {
-                ROS_DEBUG_NAMED("cyborg_coordinator",
-                    "releaseControl: %s tried to give up control, but did not have it!",
-                    request.id.c_str());
-            }
+                        currentControllerId_.clear();
+                        rosAriaCmdVelSubscriber_.shutdown();
+                    }
+                    else
+                    {
+                        ROS_DEBUG_NAMED("cyborg_coordinator",
+                            "releaseControl: %s tried to give up control, but did not have it!",
+                            request.id.c_str());
+                    }
 
-            return true;
-        }
+                    return true;
+                }
 
-    private:
-        ros::NodeHandle&   node_;
-        std::string        currentControllerId_;
-        ros::Publisher     rosAriaCmdVelPublisher_;
-        ros::Subscriber    rosAriaCmdVelSubscriber_;
-        ros::ServiceServer requestControlService_;
-        ros::ServiceServer releaseControlService_;
-};
+            private:
+                ros::NodeHandle&   node_;
+                std::string        currentControllerId_;
+                ros::Publisher     rosAriaCmdVelPublisher_;
+                ros::Subscriber    rosAriaCmdVelSubscriber_;
+                ros::ServiceServer requestControlService_;
+                ros::ServiceServer releaseControlService_;
+        };
+    }
+}
 
 int main(int argc, char** argv)
 {
     ros::init(argc, argv, "cyborg_coordinator");
 
-    ros::NodeHandle node;
-    Coordinator     coordinator(node);
+    ros::NodeHandle                  node;
+    cyborg::coordinator::Coordinator coordinator(node);
 
     ROS_DEBUG_NAMED("cyborg_coordinator", "cyborg_coordinator ready to serve!");
 
